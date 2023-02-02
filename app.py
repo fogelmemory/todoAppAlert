11e25384993b96bdc7da2863d08dedb9e0d6e156
fogelmemory <115185078+fogelmemory@users.noreply.github.com>
2023-02-02 12:29:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,40 +23,6 @@ app.config['MYSQL_DATABASE_DB'] = 'todo'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-def telegram():
-    # TOKEN = "6198018759:AAFp7ihi91iZURC15QGn4cTB81qkXFYd62k" 
-    # chat_id = "1983435776"
-    # message = "hello from your telegram bot"
-    # url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-    # print(requests.get(url).json())
-    now = datetime.now()
-    print("now =", now)
-    dt_string = now.strftime("%m-%d %H:%M")
-   
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * FROM tasks")
-    data = cursor.fetchall()
-    users = []
-    for row in data:
-        print(row[3])
-        x = row[3].split("T")
-        x[0] = x[0][5:]
-        time = x[0]+" "+x[1]
-        print(time)
-        print("date and time =", dt_string)
-        if time == dt_string:
-              TOKEN = "6198018759:AAFp7ihi91iZURC15QGn4cTB81qkXFYd62k" 
-              chat_id = "1983435776"
-              message = row[1]+" "+row[2]
-              url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-              print(requests.get(url).json())
-              time.sleep(60)
-
-  
-#  sched = BackgroundScheduler(daemon=True)
-# sched.add_job(telegram,'interval',minutes=0.1)
-#     sched.start()
-
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -114,13 +80,6 @@ def updateTasks(id,color):
     response = jsonify({'message': 'Task updated successfully'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
-@app.route("/mail")
-def email():
-    sched = BackgroundScheduler(daemon=True)
-    sched.add_job(telegram,'interval',minutes=0.1)
-    sched.start()
-    return "sent"
 
   
 if __name__ == '__main__':
